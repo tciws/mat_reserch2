2f4060c1b67c99611990b5cf1fee42f7a25f6ffd
Yuki kawaguchi <kawaguti@matlab.nitech.ac.jp>
2018-06-07 12:21:45 +0900

    add cloud

---
+++ cloud/client.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>                             
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <unistd.h>  
+
+#define BUFFSIZE 1024 
+
+int main(int argc, char* argv[]){
+  char    *host = "mikasa";                /* 相手ホスト名 */
+  int     port = 55555;                 /* 相手ポート番号 */
+  int     sockfd;               /* ソケット記述子 */
+  struct sockaddr_in      addr, my_addr;                  
+  /* インタネットソケットアドレス構造体 */
+  int     addrlen;                                    
+  char    BUFF[BUFFSIZE];       /* 送信バッファ */
+  int     nbytes;               /* 送信メッセージ長 */
+  struct hostent  *hp;          /* 相手ホストエントリ */
+  
+  if(argc != 2){
+    printf("please input 10 filename\n");
+    exit(1);
+  }
+
+  /* 相手ホストエントリの取得 */                    
+  if ((hp = gethostbyname(host)) == NULL) {
+    perror("gethostbyname");  /* 相手ホストエントリ取得失敗 */
+    exit(1);                                   
+  }      
+
+  /* 相手アドレスと相手ポート番号の設定 */             
+    bzero((char *)&addr, sizeof (addr));            /* 0クリア */
+    addr.sin_family = AF_INET;                      /* アドレスファミリ */
+    bcopy(hp->h_addr, (char *)&addr.sin_addr, hp->h_length);
+                                                    /* アドレス */
+    addr.sin_port = htons(port);                    /* ポート番号 */
+                                                           
+    /* ソケットの生成 */                                              
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("ソケット生成失敗");         /* ソケットの生成失敗 */
+        exit(1);                                        
+    }                                                   
+                                                  
+    /* 自アドレスと自ポート番号の設定 */
+    bzero((char *) &my_addr, sizeof(my_addr));      /* 0クリア */
+    my_addr.sin_family = AF_INET;                   /* アドレスファミリ */
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);    /* アドレス */
+    my_addr.sin_port = htons(0);                    /* ポート番号 */
+    if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(my_addr)) < 0) {
+        perror("設定失敗");         /* 自アドレスと自ポート番号の設定失敗 */
+        exit(1);                          
+    }                                      
+                                       
+    addrlen = sizeof (addr);                 
+
+    /* サーバとのコネクション確立 */
+    if (connect(sockfd, (struct sockaddr *)&addr, addrlen) < 0) {
+      perror("コネクション確立");
+      exit(1);
+    }
+
+    //printf("Send message: ");
+
+    FILE *fp;//読み込んだファイルへのポインタ
+    char *filename;//読み込むファイル名(標準入力から与えられる)
+    int i;
+    
+    for(i=1;i<argc;i++){
+      filename = argv[i];
+      if((fp=fopen(filename, "r")) == NULL){
+	perror("can't open file\n");
+	exit(1);
+      }
+      
+      bzero(&BUFF,sizeof(BUFF));
+      
+      while(feof(fp) == 0){
+	fgets(BUFF, BUFFSIZE, fp); /* 送信メッセージの取得 */        
+	nbytes = strlen(BUFF);        /* 送信メッセージ長の設定 */      
+	//BUFF[nbytes-1]='\0';    /* fgetsで取り込まれた文字列の最後から改行を削除 */
+	/* 送信 */                                 
+	if (send(sockfd, BUFF, nbytes, 0) != nbytes) {
+	  perror("送信失敗");         /* 送信失敗 */
+	  exit(1);                               
+	}
+	bzero(&BUFF,sizeof(BUFF));
+      }
+      fclose(fp);
+
+
+      
+    }
+    close(sockfd);
+}
---
+++ cloud/manager.c
@@ -0,0 +1,99 @@
+/* TCPのサーバプログラム */
+
+/* 以下のヘッダファイルは/usr/includeにある */
+#include        <stdio.h>                                          
+#include        <sys/types.h>                                      
+#include        <sys/socket.h>                            
+#include        <netinet/in.h>                                    
+#include	<string.h>
+#include	<strings.h>
+#include	<stdlib.h>
+#include	<unistd.h>                                                                      
+#define BUFFSIZE 1024            /* メッセージの最大長 */       
+#define port_file "mngnode.conf" //ポート番号が書いてある設定ファイル
+
+int main(void)                                              
+{                                                             
+    int     port;                 /* 自ポート番号*/
+    char    port_moji[6];
+    int     sockfd, acc_sockfd;   /* ソケット記述子 */
+    struct sockaddr_in      addr, my_addr;
+                                  /* インタネットソケットアドレス構造体 */
+    int     addrlen;                                             
+    char    BUFF[BUFFSIZE];       /* 受信バッファ */
+    int     nbytes;               /* 受信メッセージ長 */
+    //----------------自ポート番号の取得-------------------------
+    FILE *fp_port;
+    if((fp_port = fopen(port_file, "r")) == NULL){
+      perror("can't open file\n");
+      exit(1);
+    }
+    fgets(port_moji, 6, fp_port);
+    port = atoi(port_moji);
+
+    fclose(fp_port);
+    printf("my port is %d\n",port);
+    //----------------------------------------------------------
+    
+    /* ソケットの生成 */                                              
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("ソケット生成失敗");         /* ソケットの生成失敗 */
+        exit(1);                                              
+    }                                                       
+                                                             
+    /* 自アドレスと自ポート番号の設定 */
+    bzero((char *) &my_addr, sizeof(my_addr));      /* 0クリア */
+    my_addr.sin_family = AF_INET;                   /* アドレスファミリ */
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);    /* アドレス */
+    my_addr.sin_port = htons(port);                 /* ポート番号 */
+    if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(my_addr)) < 0) {
+        perror("設定失敗");         /* 自アドレスと自ポート番号の設定失敗 */
+        exit(1);                                  
+    }                                          
+                                                   
+    /* ソケットを接続待ちの状態にする */
+    listen(sockfd, 5);
+
+    addrlen = sizeof (addr);                         
+    bzero(BUFF, sizeof(BUFF));    /* 受信バッファの0クリア */
+
+    /* クライアントからの接続受付 */
+    if ((acc_sockfd = accept(sockfd, (struct sockaddr *)&addr, &addrlen)) < 0) {
+      perror("wait accept");
+      exit(1);
+    }
+
+    bzero(&BUFF,sizeof(BUFF));
+    
+    int readsize;
+    FILE *fp_write;
+    char *filename_write = "a.log";
+
+    char value[15];
+    
+    /* コピー先ファイルのオープン */    
+    //if ((fp_write = fopen(filename_write, "w")) == NULL) {
+    //	perror("can't open recvfile");
+    //	exit(1);
+    //}
+    
+    while((readsize = recv(acc_sockfd, BUFF, 512,0)) != 0){
+      printf("%s",BUFF);
+      //  if(fwrite(&BUFF, readsize, 1, fp_write) != 1){
+      //perror("fwrite");
+      //}	
+      bzero(&BUFF,sizeof(BUFF));
+    }
+    //close(fp_write);
+    
+    
+    
+
+
+
+    
+    close(acc_sockfd);  /* ソケット記述子の削除 */
+
+    close(sockfd);      /* ソケットの終了 */
+
+}
